d19a3d5e8e9bf4c16c6637832239576c54b3f20d
Dinesh <dr975208@gmail.com>
2026-01-04 23:35:49 +0530

    initial commit

---
+++ 01/script.js
@@ -0,0 +1,2 @@
+console.log('this is hello from external js file')
+console.log("hey there")
---
+++ 02/script.js
@@ -0,0 +1,50 @@
+// variales
+// Datatype Variable set some value //Strongly Typed Language
+//Javascript Is A Loosly Typed Language
+//var
+//var -> Global Scope
+// var a = 22;
+// var num1 = 10;
+// var num2 = 20;
+// var a="hello"
+// console.log(a)
+// console.log(num1 + num2 )  
+// console.log(a)
+// var firstName="Piyush"
+
+//Naming Conventions
+//firstname
+//first_name
+//firstName (pref) camelCase classes
+//Firstname
+//Any type of spaces not allowed no hyfen allowed
+
+// if(true){
+//     var age=30;
+//     console.log(age)
+// }
+// console.log(age)
+// console.log(ram)
+// var ram="sita"
+// console.log(ram)
+
+// Let
+let age =22;
+age ="Piyush"
+console.log(age)
+if(true){
+    let a=30;
+    console.log(a)
+}
+// console.log(a)
+let a;
+console.log(a)
+a = 40;
+console.log(a)
+
+// const -> Local Scope
+//const value never re-assign 
+
+const b = 22;
+console.log(b)
+console.log(-a+a)
